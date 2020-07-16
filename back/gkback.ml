3a87c1e40e66d9bd322ea0f4aa58bd582ec39a05
Palle Raabjerg <github@raabjerg.mm.st>
2020-07-16 21:34:12 +0200

    Switch to Lwt for async and remove Core library. It turned out to be easier for me to figure out this way.

--- back/gkback.ml
+++ back/gkback.ml
@@ -1,19 +1,19 @@
-open Core
-open Async
+open Lwt.Infix
 open Yojson.Basic.Util
-
-open Cohttp_async
+open Cohttp_lwt_unix
 
 module Wm = struct
   module Rd = Webmachine.Rd
   module UnixClock = struct
     let now = fun () -> int_of_float (Unix.gettimeofday ())
   end
-  include Webmachine.Make(Cohttp_async.Io)(UnixClock)
+  include Webmachine.Make(Cohttp_lwt_unix__Io)(UnixClock)
 end
 
+module StringMap = Map.Make(String)
+
 class gkback conf_init = object(self)
-  inherit [Body.t] Wm.resource
+  inherit [Cohttp_lwt.Body.t] Wm.resource
 
   val configurations = conf_init
 
@@ -32,28 +32,22 @@ class gkback conf_init = object(self)
     try Wm.Rd.lookup_path_info_exn "configuration" rd with Caml.Not_found -> "Not found"
 
   method private to_json rd =
-    let _ = Map.find configurations (self#configuration) in
-    let json =
-      Printf.sprintf "hej"
-    in
-    Wm.continue (`String json) rd
+    let configuration = StringMap.find (self#configuration rd) configurations in
+    Wm.continue (`String configuration) rd
 end
 ;;
 
-let webpath = (Sys.get_argv ()).(1)
-;;
-
-let deassoc_string = (fun (assoc_string) ->
-    match assoc_string with (`String s) -> s)
+let webpath = Sys.argv.(1)
 ;;
 
 let read_layer_metadata (layer) =
   Yojson.Basic.from_file
-    (String.concat [webpath; "/layers/"; layer; "/meta.json"])
+    (String.concat "" [webpath; "/layers/"; layer; "/meta.json"])
 ;;
+
 let read_configuration configfolder configuration =
   let conftype = configuration |> member "type" |> to_string in
-  if equal_string conftype "single"
+  if conftype = "single"
   then
     (* With a single-layer configuration, we simply use the metadata for the layer,
      * and add the type and layer address from the configuration *)
@@ -61,53 +55,59 @@ let read_configuration configfolder configuration =
     let layermeta = to_assoc (read_layer_metadata conflayeraddress) in
     let assocconf = to_assoc configuration in
     (configfolder, Yojson.Basic.to_string (`Assoc (List.concat [layermeta; assocconf])))
-  else if equal_string conftype "multi"
+  else if conftype = "multi"
   then
     (* With a multi-layer configuration, we expect the necessary
      * meta-data to be written explicitly in the configuration *)
     (configfolder, Yojson.Basic.to_string configuration)
+
   else
     raise (Invalid_argument "Invalid configuration type. Must be either \"single\" or \"multi\"")
 ;;
 
 let read_configurations (configurationpath) =
   (* Read configurations from filesystem *)
-  Sys.ls_dir configurationpath
-  >>| List.map ~f:(fun (configfolder) ->
-          read_configuration configfolder
-            (Yojson.Basic.from_file
-               (String.concat
-                  [configurationpath; "/"; configfolder; "/configuration.json"])))
+  let configdirlist = Array.to_list (Sys.readdir configurationpath) in
+  let configlist =
+    List.map
+      (fun (configfolder) ->
+        read_configuration configfolder
+          (Yojson.Basic.from_file
+             (String.concat
+                "" [configurationpath; "/"; configfolder; "/configuration.json"])))
+      configdirlist
+  in
   (* Build map of json-formatted configurations *)
-  >>| List.fold_left
-        ~init:(Map.empty (module String))
-        ~f:(fun confmap configurationtuple ->
-          let (configfolder, configuration) = configurationtuple in
-          (* let _ = (* Write to file *) in *)
-          Map.add_exn confmap ~key:configfolder ~data:configuration)
+  List.fold_left
+    (fun confmap configurationtuple ->
+      let (configfolder, configuration) = configurationtuple in
+      StringMap.add configfolder configuration confmap)
+    StringMap.empty
+    configlist
 ;;
 
 let main () =
   let port = 8080 in
-  let webpath = (Sys.get_argv ()).(1) in
-  let configurations = read_configurations (String.concat [webpath; "/configurations"]) in
+  let configurations = read_configurations (String.concat "" [webpath; "/configurations"]) in
   let routes = [
       ("/mapconf/:configuration", fun () -> new gkback configurations);
     ] in
-  let handler ~body _ request =
+  let callback (_ch, _conn) request body =
     let open Cohttp in
     Wm.dispatch' routes ~body ~request
-    >>| begin function
+    >|= begin function
           | None        -> (`Not_found, Header.init (), `String "Not found", [])
           | Some result -> result
         end
-
-    >>= fun (status_code, headers, body, _) ->
-    Server.respond ~headers ~body status_code
+    >>= fun (status, headers, body, _) ->
+    Server.respond ~headers ~body ~status ()
   in
-  Server.create ~on_handler_error:`Raise (Tcp.Where_to_listen.of_port port) handler
-  >>> (fun _server ->
-    Log.Global.info "webpath: %s, gkback: listening on 0.0.0.0:%d%!" webpath port)
-;;
+  let conn_closed (ch, _conn) =
+    Printf.printf "connection %s closed\n%!"
+      (Sexplib.Sexp.to_string_hum (Conduit_lwt_unix.sexp_of_flow ch))
+  in
+  let config = Server.make ~callback ~conn_closed () in
+  Server.create ~mode:(`TCP(`Port port)) config >|= fun () ->
+  Printf.eprintf "gkback: listening on 0.0.0.0:%d%!" port
 
-Scheduler.go_main ~main ()
+let () = Lwt_main.run (main ())
